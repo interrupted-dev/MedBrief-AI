781b2e211b61ef5e87aa393939a0a3e8f536d854
interrupted.dev <118756828+interrupted-dev@users.noreply.github.com>
2025-05-24 18:04:52 +0000

    done

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ if not st.experimental_user.is_logged_in:
     if st.button("Authenticate"):
         st.login("auth0")
 
+st.json(st.experimental_user)
+st.header(f"Hello{st.experimental_user.name}")
+st.image{st.experimental_user.picture}
+
 def create_qr_code(data):
     """Generate a QR code image for the given data."""
     qr = qrcode.QRCode(
